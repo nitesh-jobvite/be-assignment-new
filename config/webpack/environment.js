0f8b206fd5bf5dd4cef62aafa19c42a0a6c6c7ad
Nitesh Kumar <nitesh.kumar@jobvite-inc.com>
2024-04-28 22:16:46 +0530

    add env.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,18 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
-module.exports = environment
+// Check if the babel loader exists
+const babelLoader = environment.loaders.get('babel');
+
+if (babelLoader) {
+  // Modify babel loader to support .jsx and .js files
+  babelLoader.test = /\.(js|jsx)$/;
+
+  // Set Babel presets
+  babelLoader.use[0].options.presets = [
+    ['@babel/preset-env', { targets: { node: 'current' } }]
+  ];
+} else {
+  console.error('Babel loader not found in webpack environment configuration.');
+}
+
+module.exports = environment;
