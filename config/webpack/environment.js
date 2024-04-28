d02d530ec3006f2c53c76c8f323843a58459de81
Nitesh Kumar <nitesh.kumar@jobvite-inc.com>
2024-04-28 22:14:08 +0530

    add env.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,3 @@
-const { environment } = require('@rails/webpacker');
+const { environment } = require('@rails/webpacker')
 
-// Add support for .jsx and .js files using Babel
-const babelLoader = environment.loaders.get('babel');
-babelLoader.test = /\.(js|jsx)$/;
-
-// Add Babel presets
-babelLoader.options.presets = [
-  ['@babel/preset-env', { targets: { node: 'current' } }]
-];
-
-module.exports = environment;
+module.exports = environment
