5ad38f5a3d7af5c9201b0c335728e7ca3f284855
Nitesh Kumar <nitesh.kumar@jobvite-inc.com>
2024-04-29 11:58:23 +0530

    Fix settlement logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,12 @@ class User < ApplicationRecord
   has_many :expense_participants
 
   def amount_owed_to_you # you are owed
+    byebug
     amount_paid_by_others = received_transactions.sum(:amount)
     expenses = Expense.where(user_id: id)
     expense_participants_owes_you = ExpenseParticipant.where(expense_id: expenses.pluck(:id)).where.not(user_id: id)
     amount_owed_from_expenses = expense_participants_owes_you.sum(:amount_paid)
+    amount_owed_to_you = amount_owed_from_expenses - amount_paid_by_others
   end
 
   def amount_you_owe # you owe
